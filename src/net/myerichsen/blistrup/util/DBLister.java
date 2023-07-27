4f46009eab8a8582e6bd3b67714be81974080c6b
michaelerichsen <michael@myerichsen.net>
2023-07-27 11:51:17 +0200

    8: Indlæsning af folketællinger
    Task-Url: https://github.com/MichaelErichsen/Blistrup/issues/issues/8

---
+++ src/net/myerichsen/blistrup/util/DBLister.java
@@ -0,0 +1,61 @@
+package net.myerichsen.blistrup.util;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Class to list contents of a BLISTRUP Derby table.
+ *
+ * @author Michael Erichsen
+ * @version 27. jul. 2023
+ *
+ */
+public class DBLister {
+
+	/**
+	 * MilRollEntryDialog method
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		final DBLister dbl = new DBLister();
+
+		try {
+			dbl.execute(args);
+		} catch (IOException | SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Worker method
+	 *
+	 * @param args
+	 * @throws GedcomParserException
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	private void execute(String[] args) throws IOException, SQLException {
+		int taeller = 0;
+		final String dbURL = "jdbc:derby:C:\\Users\\michael\\BlistrupDB";
+		final Connection conn = DriverManager.getConnection(dbURL);
+		final PreparedStatement statement = conn.prepareStatement(
+				"SELECT DISTINCT ROLLE FROM BLISTRUP.F9PERSONFAMILIEQ WHERE TYPE = 'F' ORDER BY ROLLE");
+		final ResultSet rs = statement.executeQuery();
+
+		while (rs.next()) {
+			System.out.println(rs.getString("ROLLE"));
+			taeller++;
+		}
+
+		conn.close();
+		System.out.println(taeller + " distinkte roller fundet");
+	}
+
+}
